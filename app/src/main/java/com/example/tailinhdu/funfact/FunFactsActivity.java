269f71bfbe5e5765434ab5a2ba5942e29d14f604
Tai Linh Du <linh9a0910@gmail.com>
2018-04-24 19:14:33 +0200

    button

--- app/src/main/java/com/example/tailinhdu/funfact/FunFactsActivity.java
+++ app/src/main/java/com/example/tailinhdu/funfact/FunFactsActivity.java
@@ -2,8 +2,12 @@ package com.example.tailinhdu.funfact;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import static com.example.tailinhdu.funfact.R.id.factTextView;
+import static com.example.tailinhdu.funfact.R.id.showFactButton;
 
 public class FunFactsActivity extends AppCompatActivity {
     //Declare our View variables
@@ -16,7 +20,18 @@ public class FunFactsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fun_facts);
 
         //Assign the Views from the layout file to the corresponding variables
-        factTextView = findViewById()
+        factTextView = (TextView)findViewById(R.id.factTextView);
+        showFactButton = (Button) findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //The button was clicked ,do update the fact TextView with a new fact.
+                String fact = "Ostriches can run faster than horses";
+                factTextView.setText(fact);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
 
     }
 }
